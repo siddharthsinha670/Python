e2ba2a54a87a31b93f40604c6bb475013ddf0e54
SIDDHARTH SINHA <siddharthsinha670@gmail.com>
2025-09-11 11:05:58 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,7 @@
+age=23
+old=False
+old2=True #T,F capital hona haiye nahi to erroe ho jyega
+a=None
+print(type(old))
+print(type(a))
+print(type(old2))
---
+++ variable.py
@@ -0,0 +1,23 @@
+name= "siddharth sinha"
+age=21
+price=67.98
+
+age2=age
+
+print (name)
+print (age)
+print(price)
+
+print("my name is:",name)
+print (age2)
+
+print(type(name)) #type- hmlogo ko variable ka type bta deta hai jaise ki int ,float bool etc
+print(type(age))
+print(type(price))
+
+name2='siddharth sinha'  #ham code ko single,double,trilple cort me likh skte hai valid hai.
+name3='''siddharth sinha''' #lekin hm log (double cout)("") ka use krnge.
+
+print(name)
+print(name2)
+print( name3)
